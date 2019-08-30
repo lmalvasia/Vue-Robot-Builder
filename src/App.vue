2da3807accb14297247f1f993fcb5930629320ab
Lucas Malvasia <lucas.malvasia@gmail.com>
2019-08-30 16:04:44 -0300

    Use router link element and style the active link

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,18 @@
       <nav>
         <ul>
           <li class="nav-item">
-            <img class="logo" src="./assets/build-a-bot-logo.png" alt="logo.png" />
-            Build a bot
+            <router-link active-class="activePage" class="nav-link" :to="{name: 'Home'}" exact>
+              <!-- Podemos usar el nombre de la ruta para movernos -->
+              <!-- Por defecto, el router usa como active class router-link-active.  -->
+              <!-- Podemos cambiarlo usando active-class. -->
+              <img class="logo" src="./assets/build-a-bot-logo.png" alt="logo.png" />
+              Build a bot
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link active-class="activePage" class="nav-link" :to="{name: 'Build'}" exact>
+              Build
+            </router-link>
           </li>
         </ul>
       </nav>
@@ -65,5 +75,15 @@ ul {
 .logo {
   vertical-align: middle;
   height: 30px;
+}
+.nav-link {
+  text-decoration: none;
+  color: inherit;
+}
+.router-link-active { /* router link toma por defecto esto */
+  color:white;
+}
+.activePage { /* es posible cambiarlo */
+  color:white;
 }
 </style>
