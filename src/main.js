8c209f078a2400d7e94b3eb735b2ad417402ef02
Lucas Malvasia <lucas.malvasia@gmail.com>
2019-09-02 15:04:01 -0300

    Create new vuex store

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import Router from './router';
+import Store from './store';
 
 Vue.config.productionTip = false;
 
@@ -10,4 +11,5 @@ Vue.config.productionTip = false;
 new Vue({
   render: h => h(App),
   router: Router,
+  store: Store,
 }).$mount('#app');
