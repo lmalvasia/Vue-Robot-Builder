1f8dd866c2327269328a343ae244eda74526fca4
Lucas Malvasia <lucas.malvasia@gmail.com>
2019-09-04 10:58:26 -0300

    Create custom directives

--- src/build/partSelector.vue
+++ src/build/partSelector.vue
@@ -3,11 +3,13 @@
     <img @click="showPartInfo()" :src="selectedPart.src" title="arm"/>
     <button @click="selectPreviousPart()" class="prev-selector"></button>
     <button @click="selectNextPart()" class="next-selector"></button>
-    <span class="sale" v-show="selectedPart.onSale">Sale!</span>
+    <span v-pin class="sale" v-show="selectedPart.onSale">Sale!</span>
   </div>
 </template>
 
 <script>
+import pinDirective from '../shared/pin-directive';
+
 const getPreviousValidIndex = (index, length) => {
   const deprecatedIndex = index - 1;
   return deprecatedIndex < 0 ? length - 1 : deprecatedIndex;
@@ -19,6 +21,7 @@ const getNextValidIndex = (index, length) => {
 };
 
 export default {
+  directives: { pin: pinDirective },
   props: { // Like prop type
     parts: { type: Array, required: true },
     position: {
@@ -74,9 +77,6 @@ export default {
   border: 3px solid #aaa;
 }
 .sale {
-  position: absolute;
-  bottom: 5px;
-  right: 5px;
   color: white;
   background-color: red;
   padding: 3px;
---
+++ src/shared/pin-directive.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-param-reassign */
+export default {
+  bind: (element) => {
+    // console.log('arg: ', binding.arg, 'modifiers: ', binding.modifiers);
+    element.style.position = 'absolute';
+    element.style.bottom = '5px';
+    element.style.right = '5px';
+  },
+};
