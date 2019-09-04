af92718fb8b86cf75df170fdd3c52612cea3c39a
Lucas Malvasia <lucas.malvasia@gmail.com>
2019-09-04 12:33:32 -0300

    global filters

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue';
 import Router from './router';
 import Store from './store';
 import pinDirective from './shared/pin-directive';
+import currencyFilter from './shared/currency-filter';
 
 Vue.config.productionTip = false;
 Vue.directive('pin', pinDirective);
+Vue.filter('currency', currencyFilter);
 // Creamos una instancia de Vuew y pasamos las opciones para configurar la aplicacion VUE.
 // La opcion de render, renderiza el componente App.
 
